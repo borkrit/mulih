b289174540c2916936f67b83f94cbe9ab4f03428
Vlad <vladyslav.pohorilyi@gmail.com>
2023-05-04 19:05:14 +0200

    UPD: add to script change padding Header

--- script/main.js
+++ script/main.js
@@ -4,9 +4,11 @@ const header = document.querySelector('.header');
 window.document.addEventListener('scroll', ()=>{
     if(window.scrollY > 140) {
         header.style.background = '#000000cf'
+        header.style.padding = '5px 0'
 
     }else{
         header.style.background = 'transparent'
+        header.style.padding = '32px 0'
 
     }})
 
